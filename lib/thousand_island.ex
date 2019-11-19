c78b4864469a743885b0d6cbaaf02cb8434ae2b1
Mat Trudel <mat@geeky.net>
2019-11-18 20:58:54 -0500

    Add accessor to look up local port

--- lib/thousand_island.ex
+++ lib/thousand_island.ex
@@ -1,2 +1,10 @@
 defmodule ThousandIsland do
+  def local_port(pid) do
+    {:ok, listener_pid} =
+      pid
+      |> ThousandIsland.Server.listener_pid()
+      |> ThousandIsland.Listener.listener_socket()
+
+    :inet.port(listener_pid)
+  end
 end
