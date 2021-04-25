f662ecd7707834e0f70cf0b858bbd0b505cb4c54
Mat Trudel <mat@geeky.net>
2021-04-25 15:36:04 -0400

    Version bump to 0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ThousandIsland.MixProject do
   def project do
     [
       app: :thousand_island,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
