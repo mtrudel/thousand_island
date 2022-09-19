f22a63a667b82da3412d53967843d38ead6ec5c8
Mat Trudel <mat@geeky.net>
2022-09-19 13:02:55 -0400

    Bump CI & supported version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ThousandIsland.MixProject do
     [
       app: :thousand_island,
       version: "0.5.9",
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
