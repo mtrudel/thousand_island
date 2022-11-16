feb400137ad4876b30bc29695579ff8217928a4f
Mat Trudel <mat@geeky.net>
2022-11-16 15:55:09 -0500

    Raise min supported Elixir version to 1.12+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ThousandIsland.MixProject do
     [
       app: :thousand_island,
       version: "0.5.11",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
