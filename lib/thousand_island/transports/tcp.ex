c289c57b7ecd2d3cc8561874e862448bc956c314
Mikalai Seva <mikalai.seva@gmail.com>
2023-06-01 18:19:53 +0200

    Refines types for Transport behaviour, TCP, and SSL implementations (#62)
    * Refines types for Transport behaviour, TCP, and SSL implementations
    
    * Use narrow types for transports and their behaviour

--- lib/thousand_island/transports/tcp.ex
+++ lib/thousand_island/transports/tcp.ex
@@ -12,9 +12,13 @@ defmodule ThousandIsland.Transports.TCP do
     * `{1, 2, 3, 4}` for IPv4 addresses
     * `{1, 2, 3, 4, 5, 6, 7, 8}` for IPv6 addresses
     * `:loopback` for local loopback
-    * `:any` for all interfaces (ie: `0.0.0.0`)
-    * `{:local, "/path/to/socket"}` for a Unix domain socket. If this option is used, the `port`
-      option *must* be set to `0`.
+    * `:any` for all interfaces (i.e.: `0.0.0.0`)
+    * `{:local, "/path/to/socket"}` for a Unix domain socket. If this option is used,
+      the `port` option *must* be set to `0`
+    * `:unspec` family corresponds to AF_UNSPEC and can occur if the other side has
+      no socket address
+    * `{:undefined, any_term}` family can only occur in the unlikely event of an address
+      family that the VM does not recognise.
 
   Unless overridden, this module uses the following default options:
 
@@ -37,12 +41,17 @@ defmodule ThousandIsland.Transports.TCP do
   """
 
   @type options() :: [:gen_tcp.listen_option()]
+  @typep listener_socket :: :inet.socket()
+  @typep socket() :: :inet.socket()
 
   @behaviour ThousandIsland.Transport
 
   @hardcoded_options [mode: :binary, active: false]
 
   @impl ThousandIsland.Transport
+  @spec listen(:inet.port_number(), [:inet.inet_backend() | :gen_tcp.listen_option()]) ::
+          {:ok, listener_socket()} | {:error, reason}
+        when reason: :system_limit | :inet.posix()
   def listen(port, user_options) do
     default_options = [
       backlog: 1024,
@@ -58,21 +67,35 @@ defmodule ThousandIsland.Transports.TCP do
   end
 
   @impl ThousandIsland.Transport
+  @spec accept(listener_socket()) ::
+          {:ok, socket()} | {:error, reason}
+        when reason: :closed | :system_limit | :inet.posix()
   defdelegate accept(listener_socket), to: :gen_tcp
 
   @impl ThousandIsland.Transport
+  @spec handshake(socket()) :: {:ok, socket()}
   def handshake(socket), do: {:ok, socket}
 
   @impl ThousandIsland.Transport
+  @spec controlling_process(socket(), pid()) :: :ok | {:error, reason}
+        when reason: :closed | :not_owner | :badarg | :inet.posix()
   defdelegate controlling_process(socket, pid), to: :gen_tcp
 
   @impl ThousandIsland.Transport
+  @spec recv(socket(), non_neg_integer(), timeout()) :: ThousandIsland.Transport.on_recv()
   defdelegate recv(socket, length, timeout), to: :gen_tcp
 
   @impl ThousandIsland.Transport
+  @spec send(socket(), iodata()) :: ThousandIsland.Transport.on_send()
   defdelegate send(socket, data), to: :gen_tcp
 
   @impl ThousandIsland.Transport
+  @spec sendfile(
+          socket(),
+          filename :: String.t(),
+          offset :: non_neg_integer(),
+          length :: non_neg_integer()
+        ) :: ThousandIsland.Transport.on_sendfile()
   def sendfile(socket, filename, offset, length) do
     with {:ok, fd} <- :file.open(filename, [:raw]) do
       :file.sendfile(fd, socket, offset, length, [])
@@ -80,38 +103,67 @@ defmodule ThousandIsland.Transports.TCP do
   end
 
   @impl ThousandIsland.Transport
+  @spec getopts(socket(), ThousandIsland.Transport.socket_get_options()) ::
+          ThousandIsland.Transport.on_getopts()
   defdelegate getopts(socket, options), to: :inet
 
   @impl ThousandIsland.Transport
+  @spec setopts(socket(), ThousandIsland.Transport.socket_set_options()) ::
+          ThousandIsland.Transport.on_setopts()
   defdelegate setopts(socket, options), to: :inet
 
   @impl ThousandIsland.Transport
+  @spec shutdown(socket(), ThousandIsland.Transport.way()) ::
+          ThousandIsland.Transport.on_shutdown()
   defdelegate shutdown(socket, way), to: :gen_tcp
 
   @impl ThousandIsland.Transport
+  @spec close(socket() | listener_socket()) :: :ok
   defdelegate close(socket), to: :gen_tcp
 
   @impl ThousandIsland.Transport
+  @spec local_info(socket() | listener_socket()) :: ThousandIsland.Transport.on_local_info()
   def local_info(socket) do
     case :inet.sockname(socket) do
-      {:ok, {:local, path}} -> %{address: {:local, path}, port: 0, ssl_cert: nil}
-      {:ok, {ip, port}} -> %{address: ip, port: port, ssl_cert: nil}
-      other -> other
+      {:ok, spec} ->
+        case spec do
+          {:local, path} -> %{address: {:local, path}, port: 0, ssl_cert: nil}
+          {:unspec, <<>>} -> %{address: :unspec, port: 0, ssl_cert: nil}
+          {:undefined, term} -> %{address: {:undefined, term}, port: 0, ssl_cert: nil}
+          {ip, port} -> %{address: ip, port: port, ssl_cert: nil}
+        end
+
+      err ->
+        err
     end
   end
 
   @impl ThousandIsland.Transport
+  @spec peer_info(socket()) :: ThousandIsland.Transport.on_peer_info()
   def peer_info(socket) do
-    {:ok, {ip, port}} = :inet.peername(socket)
-    %{address: ip, port: port, ssl_cert: nil}
+    case :inet.peername(socket) do
+      {:ok, spec} ->
+        case spec do
+          {:local, path} -> %{address: {:local, path}, port: 0, ssl_cert: nil}
+          {:unspec, <<>>} -> %{address: :unspec, port: 0, ssl_cert: nil}
+          {:undefined, term} -> %{address: {:undefined, term}, port: 0, ssl_cert: nil}
+          {ip, port} -> %{address: ip, port: port, ssl_cert: nil}
+        end
+
+      err ->
+        err
+    end
   end
 
   @impl ThousandIsland.Transport
+  @spec secure?() :: false
   def secure?, do: false
 
   @impl ThousandIsland.Transport
+  @spec getstat(socket()) :: ThousandIsland.Transport.socket_stats()
   defdelegate getstat(socket), to: :inet
 
   @impl ThousandIsland.Transport
+  @spec negotiated_protocol(socket()) :: {:error, :protocol_not_negotiated}
   def negotiated_protocol(_socket), do: {:error, :protocol_not_negotiated}
 end
