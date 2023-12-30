mtrudel/thousand_island

commit d899b490a0f7ea0e9dc214dc750ac8bdeb79ecfc

feat: allow switching the transport while running (#86)
* feat: allow switching the transport while running

* Fixed a typo in test

* disabled client verify

* Move credo config to file

* Tidy up typespecs

* Move to upgrade naming

---------

Co-authored-by: Mat Trudel <mat@geeky.net>

lib/thousand_island/handler.ex
@@ -174,11 +174,16 @@ defmodule ThousandIsland.Handler do
   likely that such spans will not behave as expected.
   """
 
+  @typedoc "The possible ways to indicate a timeout when returning values to Thousand Island"
+  @type timeout_options :: timeout() | {:persistent, timeout()}
+
   @typedoc "The value returned by `c:handle_connection/2` and `c:handle_data/3`"
   @type handler_result ::
           {:continue, state :: term()}
-          | {:continue, state :: term(), timeout()}
-          | {:continue, state :: term(), {:persistent, timeout()}}
+          | {:continue, state :: term(), timeout_options()}
+          | {:switch_transport, {module(), upgrade_opts :: [term()]}, state :: term()}
+          | {:switch_transport, {module(), upgrade_opts :: [term()]}, state :: term(),
+             timeout_options()}
           | {:close, state :: term()}
           | {:error, term(), state :: term()}
 
@@ -201,6 +206,12 @@ defmodule ThousandIsland.Handler do
   is received. In order to set a persistent timeout for all future messages (essentially
   overwriting the value of `read_timeout` that was set at server startup), a value of
   `{:persistent, timeout}` may be returned.
+  * Returning `{:switch_transport, {module, opts}, state} will cause Thousand Island to try switching the transport of the
+  current socket. The `module` should be an Elixir module that implements the `ThousandIsland.Transport` behaviour.
+  Thousand Island will call `c:ThousandIsland.Transport.upgrade/2` for the given module to upgrade the transport in-place.
+  After a successful upgrade Thousand Island will switch the socket to an asynchronous mode, as if `{:continue, state}`
+  was returned. As with `:continue` return values, there are also timeout-specifying variants of
+  this return value.
   * Returning `{:error, reason, state}` will cause Thousand Island to close the socket & call the `c:handle_error/3` callback to
   allow final cleanup to be done.
   """
@@ -284,7 +295,6 @@ defmodule ThousandIsland.Handler do
 
   @spec __using__(any) :: Macro.t()
   defmacro __using__(_opts) do
-    # credo:disable-for-next-line Credo.Check.Refactor.LongQuoteBlocks
     quote location: :keep do
       @behaviour ThousandIsland.Handler
 
@@ -442,6 +452,12 @@ defmodule ThousandIsland.Handler do
         do_socket_close(socket, reason)
       end
 
+      # Called if the socket encountered an error during upgrading
+      def terminate({:shutdown, {:upgrade, reason}}, {socket, state}) do
+        __MODULE__.handle_error(reason, socket, state)
+        do_socket_close(socket, reason)
+      end
+
       # Called if the remote end shut down the connection, or if the local end closed the
       # connection by returning a `{:close,...}` tuple (in which case the socket will be open)
       def terminate({:shutdown, reason}, {%ThousandIsland.Socket{} = socket, state}) do
@@ -500,6 +516,16 @@ defmodule ThousandIsland.Handler do
             _ = ThousandIsland.Socket.setopts(socket, active: :once)
             {:noreply, {socket, state}, timeout}
 
+          {:switch_transport, {module, upgrade_opts}, state} ->
+            handle_switch_continuation(socket, module, upgrade_opts, state, socket.read_timeout)
+
+          {:switch_transport, {module, upgrade_opts}, state, {:persistent, timeout}} ->
+            socket = %{socket | read_timeout: timeout}
+            handle_switch_continuation(socket, module, upgrade_opts, state, timeout)
+
+          {:switch_transport, {module, upgrade_opts}, state, timeout} ->
+            handle_switch_continuation(socket, module, upgrade_opts, state, timeout)
+
           {:close, state} ->
             {:stop, {:shutdown, :local_closed}, {socket, state}}
 
@@ -512,6 +538,17 @@ defmodule ThousandIsland.Handler do
             else
               {:stop, reason, {socket, state}}
             end
+        end
+      end
+
+      defp handle_switch_continuation(socket, module, upgrade_opts, state, timeout) do
+        case ThousandIsland.Socket.upgrade(socket, module, upgrade_opts) do
+          {:ok, socket} ->
+            _ = ThousandIsland.Socket.setopts(socket, active: :once)
+            {:noreply, {socket, state}, timeout}
+
+          {:error, reason} ->
+            {:stop, {:shutdown, {:upgrade, reason}}, {socket, state}}
         end
       end
     end

.credo.exs
@@ -1,0 +1,140 @@
+%{
+  #
+  configs: [
+    %{
+      name: "default",
+      strict: true,
+      checks: %{
+        enabled: [
+          #
+          ## Consistency Checks
+          #
+          {Credo.Check.Consistency.ExceptionNames, []},
+          {Credo.Check.Consistency.LineEndings, []},
+          {Credo.Check.Consistency.ParameterPatternMatching, []},
+          {Credo.Check.Consistency.SpaceAroundOperators, []},
+          {Credo.Check.Consistency.SpaceInParentheses, []},
+          {Credo.Check.Consistency.TabsOrSpaces, []},
+
+          #
+          ## Design Checks
+          #
+          {Credo.Check.Design.AliasUsage, false},
+          {Credo.Check.Design.TagTODO, [exit_status: 2]},
+          {Credo.Check.Design.TagFIXME, []},
+
+          #
+          ## Readability Checks
+          #
+          {Credo.Check.Readability.AliasOrder, []},
+          {Credo.Check.Readability.FunctionNames, []},
+          {Credo.Check.Readability.LargeNumbers, []},
+          {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 120]},
+          {Credo.Check.Readability.ModuleAttributeNames, []},
+          {Credo.Check.Readability.ModuleDoc, []},
+          {Credo.Check.Readability.ModuleNames, []},
+          {Credo.Check.Readability.ParenthesesInCondition, []},
+          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, []},
+          {Credo.Check.Readability.PipeIntoAnonymousFunctions, []},
+          {Credo.Check.Readability.PredicateFunctionNames, []},
+          {Credo.Check.Readability.PreferImplicitTry, []},
+          {Credo.Check.Readability.RedundantBlankLines, []},
+          {Credo.Check.Readability.Semicolons, []},
+          {Credo.Check.Readability.SpaceAfterCommas, []},
+          {Credo.Check.Readability.StringSigils, []},
+          {Credo.Check.Readability.TrailingBlankLine, []},
+          {Credo.Check.Readability.TrailingWhiteSpace, []},
+          {Credo.Check.Readability.UnnecessaryAliasExpansion, []},
+          {Credo.Check.Readability.VariableNames, []},
+          {Credo.Check.Readability.WithSingleClause, []},
+
+          #
+          ## Refactoring Opportunities
+          #
+          {Credo.Check.Refactor.Apply, []},
+          {Credo.Check.Refactor.CondStatements, []},
+          {Credo.Check.Refactor.CyclomaticComplexity, [max_complexity: 15]},
+          {Credo.Check.Refactor.FunctionArity, []},
+          {Credo.Check.Refactor.MatchInCondition, []},
+          {Credo.Check.Refactor.MapJoin, []},
+          {Credo.Check.Refactor.NegatedConditionsInUnless, []},
+          {Credo.Check.Refactor.NegatedConditionsWithElse, []},
+          {Credo.Check.Refactor.Nesting, [max_nesting: 3]},
+          {Credo.Check.Refactor.UnlessWithElse, []},
+          {Credo.Check.Refactor.WithClauses, []},
+          {Credo.Check.Refactor.FilterCount, []},
+          {Credo.Check.Refactor.FilterFilter, []},
+          {Credo.Check.Refactor.RejectReject, []},
+
+          #
+          ## Warnings
+          #
+          {Credo.Check.Warning.ApplicationConfigInModuleAttribute, []},
+          {Credo.Check.Warning.BoolOperationOnSameValues, []},
+          {Credo.Check.Warning.Dbg, []},
+          {Credo.Check.Warning.ExpensiveEmptyEnumCheck, []},
+          {Credo.Check.Warning.IExPry, []},
+          {Credo.Check.Warning.IoInspect, []},
+          {Credo.Check.Warning.MissedMetadataKeyInLoggerConfig, []},
+          {Credo.Check.Warning.OperationOnSameValues, []},
+          {Credo.Check.Warning.OperationWithConstantResult, []},
+          {Credo.Check.Warning.RaiseInsideRescue, []},
+          {Credo.Check.Warning.SpecWithStruct, []},
+          {Credo.Check.Warning.WrongTestFileExtension, []},
+          {Credo.Check.Warning.UnusedEnumOperation, []},
+          {Credo.Check.Warning.UnusedFileOperation, []},
+          {Credo.Check.Warning.UnusedKeywordOperation, []},
+          {Credo.Check.Warning.UnusedListOperation, []},
+          {Credo.Check.Warning.UnusedPathOperation, []},
+          {Credo.Check.Warning.UnusedRegexOperation, []},
+          {Credo.Check.Warning.UnusedStringOperation, []},
+          {Credo.Check.Warning.UnusedTupleOperation, []},
+          {Credo.Check.Warning.UnsafeExec, []}
+        ],
+        disabled: [
+          #
+          # Controversial and experimental checks (opt-in, just move the check to `:enabled`
+          #   and be sure to use `mix credo --strict` to see low priority checks)
+          #
+          {Credo.Check.Consistency.MultiAliasImportRequireUse, []},
+          {Credo.Check.Consistency.UnusedVariableNames, []},
+          {Credo.Check.Design.DuplicatedCode, []},
+          {Credo.Check.Design.SkipTestWithoutComment, []},
+          {Credo.Check.Readability.AliasAs, []},
+          {Credo.Check.Readability.BlockPipe, []},
+          {Credo.Check.Readability.ImplTrue, []},
+          {Credo.Check.Readability.MultiAlias, []},
+          {Credo.Check.Readability.NestedFunctionCalls, []},
+          {Credo.Check.Readability.OneArityFunctionInPipe, []},
+          {Credo.Check.Readability.SeparateAliasRequire, []},
+          {Credo.Check.Readability.SingleFunctionToBlockPipe, []},
+          {Credo.Check.Readability.SinglePipe, []},
+          {Credo.Check.Readability.Specs, []},
+          {Credo.Check.Readability.StrictModuleLayout, []},
+          {Credo.Check.Readability.WithCustomTaggedTuple, []},
+          {Credo.Check.Readability.OnePipePerLine, []},
+          {Credo.Check.Refactor.ABCSize, []},
+          {Credo.Check.Refactor.AppendSingleItem, []},
+          {Credo.Check.Refactor.DoubleBooleanNegation, []},
+          {Credo.Check.Refactor.FilterReject, []},
+          {Credo.Check.Refactor.IoPuts, []},
+          {Credo.Check.Refactor.MapMap, []},
+          {Credo.Check.Refactor.ModuleDependencies, []},
+          {Credo.Check.Refactor.NegatedIsNil, []},
+          {Credo.Check.Refactor.PassAsyncInTestCases, []},
+          {Credo.Check.Refactor.PipeChainStart, []},
+          {Credo.Check.Refactor.RedundantWithClauseResult, []},
+          {Credo.Check.Refactor.RejectFilter, []},
+          {Credo.Check.Refactor.VariableRebinding, []},
+          {Credo.Check.Warning.LazyLogging, []},
+          {Credo.Check.Warning.LeakyEnvironment, []},
+          {Credo.Check.Warning.MapGetUnsafePass, []},
+          {Credo.Check.Warning.MixEnv, []},
+          {Credo.Check.Warning.UnsafeToAtom, []}
+
+          # {Credo.Check.Refactor.MapInto, []},
+        ]
+      }
+    }
+  ]
+}

lib/thousand_island/socket.ex
@@ -51,6 +51,26 @@ defmodule ThousandIsland.Socket do
   end
 
   @doc """
+  Upgrades the transport of the socket to use the specified transport module, performing any client
+  handshaking that may be required. The passed options are blindly passed through to the new
+  transport module.
+
+  This is normally called internally by `ThousandIsland.Handler` and does not need to be
+  called by implementations which are based on `ThousandIsland.Handler`
+  """
+  @spec upgrade(t(), module(), term()) :: ThousandIsland.Transport.on_upgrade()
+  def upgrade(%__MODULE__{} = socket, module, opts) when is_atom(module) do
+    case module.upgrade(socket.socket, opts) do
+      {:ok, updated_socket} ->
+        {:ok, %__MODULE__{socket | socket: updated_socket, transport_module: module}}
+
+      {:error, reason} = err ->
+        ThousandIsland.Telemetry.stop_span(socket.span, %{}, %{error: reason})
+        err
+    end
+  end
+
+  @doc """
   Returns available bytes on the given socket. Up to `length` bytes will be
   returned (0 can be passed in to get the next 'available' bytes, typically the
   next packet). If insufficient bytes are available, the function can wait `timeout`

lib/thousand_island/transport.ex
@@ -41,11 +41,32 @@ defmodule ThousandIsland.Transport do
   @typedoc "The direction in which to shutdown a connection in advance of closing it"
   @type way() :: :read | :write | :read_write
 
-  @typedoc "The return value from a getopts/2 call"
-  @type on_getopts() :: {:ok, [:inet.socket_optval()]} | {:error, :inet.posix()}
+  @typedoc "The return value from a listen/2 call"
+  @type on_listen() ::
+          {:ok, listener_socket()} | {:error, :system_limit} | {:error, :inet.posix()}
 
-  @typedoc "The return value from a setopts/2 call"
-  @type on_setopts() :: :ok | {:error, :inet.posix()}
+  @typedoc "The return value from an accept/1 call"
+  @type on_accept() :: {:ok, socket()} | {:error, on_accept_tcp_error() | on_accept_ssl_error()}
+
+  @type on_accept_tcp_error() :: :closed | :system_limit | :inet.posix()
+  @type on_accept_ssl_error() :: :closed | :timeout | :ssl.error_alert()
+
+  @typedoc "The return value from a controlling_process/2 call"
+  @type on_controlling_process() :: :ok | {:error, :closed | :not_owner | :badarg | :inet.posix()}
+
+  @typedoc "The return value from a handshake/1 call"
+  @type on_handshake() :: {:ok, socket()} | {:error, on_handshake_ssl_error()}
+
+  @type on_handshake_ssl_error() :: :closed | :timeout | :ssl.error_alert()
+
+  @typedoc "The return value from a upgrade/2 call"
+  @type on_upgrade() :: {:ok, socket()} | {:error, term()}
+
+  @typedoc "The return value from a shutdown/2 call"
+  @type on_shutdown() :: :ok | {:error, :inet.posix()}
+
+  @typedoc "The return value from a close/1 call"
+  @type on_close() :: :ok | {:error, any()}
 
   @typedoc "The return value from a recv/3 call"
   @type on_recv() :: {:ok, binary()} | {:error, :closed | :timeout | :inet.posix()}
@@ -58,23 +79,11 @@ defmodule ThousandIsland.Transport do
           {:ok, non_neg_integer()}
           | {:error, :inet.posix() | :closed | :badarg | :not_owner | :eof}
 
-  @typedoc "The return value from an accept/1 call"
-  @type on_accept() :: {:ok, socket()} | {:error, on_accept_tcp_error() | on_accept_ssl_error()}
+  @typedoc "The return value from a getopts/2 call"
+  @type on_getopts() :: {:ok, [:inet.socket_optval()]} | {:error, :inet.posix()}
 
-  @type on_accept_tcp_error() :: :closed | :system_limit | :inet.posix()
-  @type on_accept_ssl_error() :: :closed | :timeout | :ssl.error_alert()
-
-  @typedoc "The return value from a handshake/1 call"
-  @type on_handshake() ::
-          {:ok, socket()} | {:ok, socket(), any()} | {:error, on_handshake_ssl_error()}
-
-  @type on_handshake_ssl_error() :: :closed | :timeout | :ssl.error_alert()
-
-  @typedoc "The return value from a shutdown/2 call"
-  @type on_shutdown() :: :ok | {:error, :inet.posix()}
-
-  @typedoc "The return value from a close/1 call"
-  @type on_close() :: :ok | {:error, any()}
+  @typedoc "The return value from a setopts/2 call"
+  @type on_setopts() :: :ok | {:error, :inet.posix()}
 
   @typedoc "The return value from a sockname/1 call"
   @type on_sockname() :: {:ok, socket_info()} | {:error, :inet.posix()}
@@ -111,11 +120,17 @@ defmodule ThousandIsland.Transport do
   @callback handshake(socket()) :: on_handshake()
 
   @doc """
+  Performs an upgrade of an existing client connection (for example upgrading
+  an already-established connection to SSL). Transports which do not support upgrading can return
+  `{:error, :unsupported_upgrade}`.
+  """
+  @callback upgrade(socket(), term()) :: on_upgrade()
+
+  @doc """
   Transfers ownership of the given socket to the given process. This will always
   be called by the process which currently owns the socket.
   """
-  @callback controlling_process(socket(), pid()) ::
-              :ok | {:error, :closed | :not_owner | :badarg | :inet.posix()}
+  @callback controlling_process(socket(), pid()) :: on_controlling_process()
 
   @doc """
   Returns available bytes on the given socket. Up to `num_bytes` bytes will be

lib/thousand_island/transports/ssl.ex
@@ -48,8 +48,7 @@ defmodule ThousandIsland.Transports.SSL do
 
   @impl ThousandIsland.Transport
   @spec listen(:inet.port_number(), [:ssl.tls_server_option()]) ::
-          {:ok, listener_socket()} | {:error, reason}
-        when reason: :system_limit | :inet.posix()
+          ThousandIsland.Transport.on_listen()
   def listen(port, user_options) do
     default_options = [
       backlog: 1024,
@@ -74,19 +73,29 @@ defmodule ThousandIsland.Transports.SSL do
   end
 
   @impl ThousandIsland.Transport
-  @spec accept(listener_socket()) :: {:ok, socket()} | {:error, reason}
-        when reason: :closed | :system_limit | :inet.posix()
+  @spec accept(listener_socket()) :: ThousandIsland.Transport.on_accept()
   defdelegate accept(listener_socket), to: :ssl, as: :transport_accept
 
   @impl ThousandIsland.Transport
-  @spec handshake(socket()) ::
-          {:ok, socket()} | {:ok, socket(), :ssl.protocol_extensions()} | {:error, reason}
-        when reason: :closed | :timeout | :ssl.error_alert()
-  defdelegate handshake(socket), to: :ssl
+  @spec handshake(socket()) :: ThousandIsland.Transport.on_handshake()
+  def handshake(socket) do
+    case :ssl.handshake(socket) do
+      {:ok, socket, _protocol_extensions} -> {:ok, socket}
+      other -> other
+    end
+  end
 
   @impl ThousandIsland.Transport
-  @spec controlling_process(socket(), pid()) :: :ok | {:error, reason}
-        when reason: :closed | :not_owner | :badarg | :inet.posix()
+  @spec upgrade(socket(), options()) :: ThousandIsland.Transport.on_upgrade()
+  def upgrade(socket, opts) do
+    case :ssl.handshake(socket, opts) do
+      {:ok, socket, _protocol_extensions} -> {:ok, socket}
+      other -> other
+    end
+  end
+
+  @impl ThousandIsland.Transport
+  @spec controlling_process(socket(), pid()) :: ThousandIsland.Transport.on_controlling_process()
   defdelegate controlling_process(socket, pid), to: :ssl
 
   @impl ThousandIsland.Transport

lib/thousand_island/transports/tcp.ex
@@ -46,8 +46,7 @@ defmodule ThousandIsland.Transports.TCP do
 
   @impl ThousandIsland.Transport
   @spec listen(:inet.port_number(), [:inet.inet_backend() | :gen_tcp.listen_option()]) ::
-          {:ok, listener_socket()} | {:error, reason}
-        when reason: :system_limit | :inet.posix()
+          ThousandIsland.Transport.on_listen()
   def listen(port, user_options) do
     default_options = [
       backlog: 1024,
@@ -63,18 +62,19 @@ defmodule ThousandIsland.Transports.TCP do
   end
 
   @impl ThousandIsland.Transport
-  @spec accept(listener_socket()) ::
-          {:ok, socket()} | {:error, reason}
-        when reason: :closed | :system_limit | :inet.posix()
+  @spec accept(listener_socket()) :: ThousandIsland.Transport.on_accept()
   defdelegate accept(listener_socket), to: :gen_tcp
 
   @impl ThousandIsland.Transport
-  @spec handshake(socket()) :: {:ok, socket()}
+  @spec handshake(socket()) :: ThousandIsland.Transport.on_handshake()
   def handshake(socket), do: {:ok, socket}
 
   @impl ThousandIsland.Transport
-  @spec controlling_process(socket(), pid()) :: :ok | {:error, reason}
-        when reason: :closed | :not_owner | :badarg | :inet.posix()
+  @spec upgrade(socket(), options()) :: ThousandIsland.Transport.on_upgrade()
+  def upgrade(_, _), do: {:error, :unsupported_upgrade}
+
+  @impl ThousandIsland.Transport
+  @spec controlling_process(socket(), pid()) :: ThousandIsland.Transport.on_controlling_process()
   defdelegate controlling_process(socket, pid), to: :gen_tcp
 
   @impl ThousandIsland.Transport
@@ -134,7 +134,7 @@ defmodule ThousandIsland.Transports.TCP do
   defdelegate peername(socket), to: :inet
 
   @impl ThousandIsland.Transport
-  @spec peercert(socket()) :: {:error, :not_secure}
+  @spec peercert(socket()) :: ThousandIsland.Transport.on_peercert()
   def peercert(_socket), do: {:error, :not_secure}
 
   @impl ThousandIsland.Transport
@@ -146,6 +146,6 @@ defmodule ThousandIsland.Transports.TCP do
   defdelegate getstat(socket), to: :inet
 
   @impl ThousandIsland.Transport
-  @spec negotiated_protocol(socket()) :: {:error, :protocol_not_negotiated}
+  @spec negotiated_protocol(socket()) :: ThousandIsland.Transport.on_negotiated_protocol()
   def negotiated_protocol(_socket), do: {:error, :protocol_not_negotiated}
 end

test/thousand_island/handler_test.exs
@@ -174,6 +174,96 @@ defmodule ThousandIsland.HandlerTest do
     end
   end
 
+  describe "upgrade" do
+    defmodule HandleConnection.UpgradingEcho do
+      use ThousandIsland.Handler
+
+      require Logger
+
+      @impl ThousandIsland.Handler
+      def handle_connection(socket, state) do
+        ThousandIsland.Socket.send(socket, "HELLO")
+
+        {:switch_transport,
+         {ThousandIsland.Transports.SSL,
+          certfile: Path.join(__DIR__, "../support/cert.pem"),
+          keyfile: Path.join(__DIR__, "../support/key.pem")}, state}
+      end
+
+      @impl ThousandIsland.Handler
+      def handle_data(data, socket, state) do
+        ThousandIsland.Socket.send(socket, data)
+        {:continue, state}
+      end
+    end
+
+    test "it should allow upgrading the transport mid-connection when supported" do
+      {:ok, port} = start_handler(HandleConnection.UpgradingEcho)
+
+      assert {:ok, client} = :gen_tcp.connect(:localhost, port, [active: false], 100)
+      assert :gen_tcp.recv(client, 0) == {:ok, ~c"HELLO"}
+
+      assert {:ok, client} =
+               :ssl.connect(
+                 client,
+                 [cacertfile: Path.join(__DIR__, "../support/ca.pem"), verify: :verify_none],
+                 100
+               )
+
+      # Check that echo works over new transport
+      :ssl.send(client, "Test me")
+      assert :ssl.recv(client, 0) == {:ok, ~c"Test me"}
+    end
+
+    test "it should handle upgrade errors" do
+      {:ok, port} =
+        start_handler(HandleConnection.UpgradeError,
+          transport_module: ThousandIsland.Transports.SSL,
+          transport_options: [
+            certfile: Path.join(__DIR__, "../support/cert.pem"),
+            keyfile: Path.join(__DIR__, "../support/key.pem"),
+            alpn_preferred_protocols: ["foo"]
+          ]
+        )
+
+      messages =
+        capture_log(fn ->
+          assert {:ok, client} =
+                   :ssl.connect(
+                     ~c"localhost",
+                     port,
+                     active: false,
+                     cacertfile: Path.join(__DIR__, "../support/ca.pem"),
+                     verify: :verify_peer
+                   )
+
+          assert :ssl.recv(client, 0) == {:ok, ~c"HELLO"}
+          Process.sleep(100)
+        end)
+
+      # Ensure that we saw the message displayed by the handle_error callback
+      assert messages =~ "handle_error: unsupported_upgrade"
+    end
+
+    defmodule HandleConnection.UpgradeError do
+      use ThousandIsland.Handler
+
+      require Logger
+
+      @impl ThousandIsland.Handler
+      def handle_connection(socket, state) do
+        ThousandIsland.Socket.send(socket, "HELLO")
+
+        {:switch_transport, {ThousandIsland.Transports.TCP, []}, state}
+      end
+
+      @impl ThousandIsland.Handler
+      def handle_error(error, _socket, _state) do
+        Logger.error("handle_error: #{error}")
+      end
+    end
+  end
+
   describe "handle_data" do
     defmodule HandleData.HelloWorld do
       use ThousandIsland.Handler

commit fd336ab312f115403b7855b849e721dd35e93699

Quell downstream dialyzer error introduced by #86

lib/thousand_island/handler.ex
@@ -541,6 +541,7 @@ defmodule ThousandIsland.Handler do
         end
       end
 
+      @dialyzer {:nowarn_function, handle_switch_continuation: 5}
       defp handle_switch_continuation(socket, module, upgrade_opts, state, timeout) do
         case ThousandIsland.Socket.upgrade(socket, module, upgrade_opts) do
           {:ok, socket} ->